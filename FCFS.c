7ba3c0058e6d00ea101dc3d65df47b4c677a554e
204g1a0526 <94513520+204g1a0526@users.noreply.github.com>
2022-10-11 11:21:47 +0530

    Created FCFS.c

---
+++ FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: T.Chandana
+   Roll No: 204G1A0526
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 10-10-2022
+   Date of Execution: 10-10-2022
+*/
